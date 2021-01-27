68143f27a42d0245edc09c42524ac83526f8f18d
Stanislav Marochok <ssssstas30@gmail.com>
2021-01-27 12:32:55 +0200

    added sign up and sign in and sign out with jwt token

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -6,8 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -17,7 +18,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'username',
         'email',
         'password',
     ];
@@ -40,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims() {
+        return [];
+    }    
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JwtAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'auth'
+], function ($router) {
+    Route::get('/user',           [JwtAuthController::class, 'user']);
+
+    Route::post('/signup',        [JwtAuthController::class, 'register']);
+    Route::post('/signin',        [JwtAuthController::class, 'login']);
+    Route::post('/token-refresh', [JwtAuthController::class, 'refresh']);
+    Route::post('/signout',       [JwtAuthController::class, 'signout']);
 });
